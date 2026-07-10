a2a9e718bf5b6c49ca4e88927d90b747023ec76b
himanshubhatnagar2000 <himanshu2000b@gmail.com>
2026-07-10 16:43:31 +0530

    feat: implement Footer component with value proposition banner and newsletter subscription form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -96,7 +96,7 @@ export default function Footer() {
 
       {/* Main Footer */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-14">
-        <div className="flex flex-row justify-between gap-10">
+        <div className="flex lg:flex-row md:flex-row flex-col justify-between gap-10">
 
           {/* Brand Column */}
           <div className="lg:col-span-1 flex flex-col gap-5">
